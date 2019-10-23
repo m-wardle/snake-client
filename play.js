75136a8fc21315d365b1bdbefb4da5aca61aff7c
Matthew Wardle <mwardle.22@gmail.com>
2019-10-23 17:50:34 +0000

    Move new input functions to separate input.js, export, and require in play.js

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      process.stdout.write('.');
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  }
-  handleUserInput();
-  return stdin;
-}
 
 setupInput();
