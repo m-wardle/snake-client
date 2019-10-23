78ca05a17abc7a937d2dede7724de4b1c3e5f7ce
Matthew Wardle <mwardle.22@gmail.com>
2019-10-23 17:47:39 +0000

    Add handleUserInput function with functionality to quit using ctrl+c

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      process.stdout.write('.');
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  }
+  handleUserInput();
+  return stdin;
+}
+
+setupInput();
