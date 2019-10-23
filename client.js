54ca41710f5f0bca6825bf8d60b47cf35dd95498
Matthew Wardle <mwardle.22@gmail.com>
2019-10-23 16:30:01 +0000

    Added Name submission to client.js

--- client.js
+++ client.js
@@ -6,7 +6,13 @@ const connect = function() {
     port: 50541
   });
   // interpret incoming data as text
+  conn.on("connect", () => {
+    console.log("Successfully connected!")
+  })
   conn.setEncoding('utf8'); 
+  conn.on("connect", () => {
+    conn.write("Name: LUL");
+  })
   conn.on("data", (data) => {
     console.log("Server says: ", data)
   });
