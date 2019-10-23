4d86e1d98642fb20624bb3655bc4c89b94fffb61
Matthew Wardle <mwardle.22@gmail.com>
2019-10-23 17:58:56 +0000

    Add requisite functionality to accept WASD as movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -8,12 +11,21 @@ const setupInput = function() {
       process.stdout.write('.');
       if (key === '\u0003') {
         process.exit();
+      } else if (key === '\u0077') {
+        conn.write("Move: up");
+      } else if (key === '\u0061') {
+        conn.write("Move: left");
+      } else if (key === '\u0073') {
+        conn.write("Move: down");
+      } else if (key === '\u0064') {
+        conn.write("Move: right");
       }
     });
   }
   handleUserInput();
   return stdin;
 }
+
 
 module.exports = {
   setupInput,
